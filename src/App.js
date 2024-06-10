726aaf6ae2b6ab75b57693a77f9b7e8aa9871619
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-06-09 21:33:50 -0400

    requests data from meme api, implements function of get new memes button

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import './App.css'
 import Header from "./components/Header"
 import Meme from "./components/Meme"
 import Footer from "./components/Footer"
+import React, {useState} from "react"
+
+
 
 function App(){
+    
+    const [state,setState] = useState(0)
+
+
     return (
         <div>
             <Header />
