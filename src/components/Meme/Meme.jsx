7f4064dcc0f7274cb1465581cbf27318de49a467
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-06-10 17:22:32 -0400

    implements feature of placing user input onto the meme image

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -10,7 +10,7 @@ function Meme(){
         "memeURL": "https://i.imgflip.com/1bij.jpg"
     })
     
-    const [allMemes, setAllMemes] = useState({})
+    const[allMemes, setAllMemes] = useState({})
 
     useEffect(() => {
         fetch('https://api.imgflip.com/get_memes')
@@ -28,21 +28,36 @@ function Meme(){
             
     })})
     }
+    function topInputChange(event){
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            "topText": event.target.value
+        }))
+    }
+
+    function bottomInputChange(event){
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            "bottomText": event.target.value
+        }))
+    }
 
     return(
         <div id="meme-container">
             <div id ="form">
                 <label>
                     Top Text: 
-                    <input id="top-text" type = "text"></input>
+                    <input onChange={topInputChange} id="top-text" type = "text"></input>
                 </label>
                 <label>
                     Bottom Text:
-                    <input type = "text"></input>
+                    <input onChange={bottomInputChange} type = "text"></input>
                 </label>
                 
                 
             </div>
+            <div id="top-text-container">{meme.topText}</div>
+            <div id ="bottom-text-container">{meme.bottomText}</div>
             <img id = "meme-image" src = {meme.memeURL} alt ="meme"></img>
             <div id ="top-two-buttons">
                 <button className = "button" onClick = {getNewImage}> GET A NEW MEME </button>
